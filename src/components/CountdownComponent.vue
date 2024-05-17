3f5925011f98490c99f3aa16b201f51053686f9d
Eithliu <iris.benoit@gmail.com>
2024-05-17 13:33:43 +0200

    created a countdown for 4 seconds

---
+++ src/components/CountdownComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <p v-if="countdown > 0" class="timer">{{ countdown }}</p>
+</template>
+
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const countdown = ref(4);
+
+const startCountdown = () => {
+    const timer = setInterval(() => {
+    countdown.value--;
+    if (countdown.value === 0) {
+        clearInterval(timer);
+    }
+}, 1000);
+
+onMounted(() => {
+    startCountdown();
+})
+
+</script>
